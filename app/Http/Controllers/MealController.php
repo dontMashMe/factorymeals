7a803fd5537268f07cf43384f173f5aa9a9e4983
Marijan Bebek <marijan.bebek@datamir.info>
2022-08-21 22:42:15 +0200

    Added validation for rest of parameters

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -11,20 +11,6 @@ class MealController extends Controller
     
     public function get(GetRequest $request)
     {
-        $request->validate([
-            'lang' => [
-                'required',
-                'size:2',
-                'string',
-                'exists:languages,locale'
-            ]
-        ]);
-
-
-    }
-
-    
-   
-    
-    
+        //
+    }    
 }
--- app/Http/Requests/GetRequest.php
+++ app/Http/Requests/GetRequest.php
@@ -26,22 +26,34 @@ class GetRequest extends FormRequest
     {
         
         return [
-            'lang' => [
+            // accept any number, NULL or !NULL
+            'category'  => 'regex:/(?i)(^([0-9]+|NULL|\!NULL)$)/',
+            'diff_time' => 'integer',
+            'with'      => 'in:ingredients,category,tags',
+            'per_page'  => 'integer',
+            'page'      => 'integer',
+            'tags'      => 'array',
+        
+            'lang'      => [
                 'required',
                 'size:2',
                 'string',
                 'exists:languages,locale'
-            ]
+            ],
         ];
     }
 
     public function messages()
     {
         return [
-            'lang.required' => "Please enter a 'lang' param value.",
-            'lang.size'     => "Lang param has to be 2 characters long.",
-            'lang.string'   => "Lang param must be characters only.",
-            'lang.size'     => "Lang param must be one of: ".implode(",", Language::GetLocales()),
+            'lang.required'     => "Please enter a 'lang' param value.",
+            'lang.size'         => "Lang param has to be 2 characters long.",
+            'lang.string'       => "Lang param must be characters only.",
+            'lang.exists'       => "Lang param must be one of: ".implode(",", Language::GetLocales()),
+            'with.in'           => "Can only contain 'ingredients', 'category' or 'tags'",
+            'per_page.integer'  => "Per_page param must be a number.",
+            'page.integer'      => "Page param must be a number.",
+            'tags.array'        => "Tags must be sent as a list. e.g &tags[]=1,2,3",
         ];
     }
 }
