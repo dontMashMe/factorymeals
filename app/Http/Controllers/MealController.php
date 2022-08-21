25be838bf6fc24a81cd7b362d5d14d4302f5276a
Marijan Bebek <marijan.bebek@datamir.info>
2022-08-22 01:07:11 +0200

    Added filtering by tags & category

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -15,8 +15,30 @@ class MealController extends Controller
     
     public function get(GetRequest $request)
     {
+        
+        // set the locale
         $this->_setlocale($request->lang);
-        return MealResource::collection(Meal::all());  
+
+        // pluck the optional filtering params out of URL
+        $category = $request->input('category');
+        $tags     = $request->input('tags');
+
+        // build the query and return collection.
+        return MealResource::collection(
+            Meal::when($category, function ($query, $category) {
+                if(is_numeric($category)) $query->where('category_id', $category);
+                elseif($category == "!NULL") $query->whereNotNull('category_id');
+                else $query->whereNull('category_id');
+            })
+            ->when($tags, function($query, $tags) {
+                $query->whereHas('tags', function($q) use($tags){
+                    $q->whereIn('id', $tags);
+                });
+            })
+            ->get()
+        
+        );
+        
     }
 
     public function _setlocale($locale)
