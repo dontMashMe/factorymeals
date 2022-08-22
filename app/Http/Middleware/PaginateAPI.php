7224bcfe121b8e1a5134ef13637e058ab47209c4
Marijan Bebek <marijan.bebek@datamir.info>
2022-08-22 02:20:14 +0200

    Cleaning up metadata in response

--- app/Http/Middleware/PaginateAPI.php
+++ app/Http/Middleware/PaginateAPI.php
@@ -16,6 +16,16 @@ class PaginateAPI
      */
     public function handle(Request $request, Closure $next)
 	{
+        /**
+         * 
+         * 
+         *   "meta": {
+            "currentPage": 2,
+            "totalItems": 8,
+            "itemsPerPage": 5,
+            "totalPages": 2
+        },
+         */
 		$response = $next($request);
 
 		$data = $response->getData(true);
@@ -30,6 +40,35 @@ class PaginateAPI
         if (isset($data['meta']['path'])){
             unset($data['meta']['path']);
         }
+
+        if (isset($data['meta']['from'])){
+            unset($data['meta']['from']);
+        }
+
+        $data['meta']['totalPages'] = $data['meta']['last_page'];
+        if (isset($data['meta']['last_page'])){
+            unset($data['meta']['last_page']);
+        }
+
+        if (isset($data['meta']['to'])){
+            unset($data['meta']['to']);
+        }
+
+        if(isset($data['meta']['total'])){
+            $data['meta']['totalItems'] = $data['meta']['total'];
+            unset($data['meta']['total']);
+        }
+
+        if(isset($data['meta']['per_page'])){
+            $data['meta']['itemsPerPage'] = $data['meta']['per_page'];
+            unset($data['meta']['per_page']);
+        }
+
+        if(isset($data['meta']['current_page'])){
+            $data['meta']['currentPage'] = $data['meta']['current_page'];
+            unset($data['meta']['current_page']);
+        }
+        
 
 		$response->setData($data);
 
